58baa279450ae04d9ab0ed42419af940b24c0d77
unkown812 <jayjogane@gmail.com>
2025-08-25 21:13:49 +0530

    new file:   .github/workflows/npm-grunt.yml 	new file:   eslint.config.js 	new file:   index.html 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	new file:   public/_redirects 	deleted:    public/logo.png 	deleted:    public/logo.svg 	deleted:    sk.zip 	modified:   src/App.tsx 	deleted:    src/assets/logo.png 	deleted:    src/assets/logo.svg 	new file:   src/components/auth/ProtectedRoute.tsx 	new file:   src/components/dashboard/AttendanceChart.tsx 	new file:   src/components/dashboard/BirthdayReminder.tsx 	new file:   src/components/dashboard/DueDateReminder.tsx 	new file:   src/components/dashboard/PerformanceWidget.tsx 	new file:   src/components/dashboard/RecentFeePayments.tsx 	new file:   src/components/dashboard/StatCard.tsx 	new file:   src/components/dashboard/UpcomingExams.tsx 	modified:   src/components/layout/Header.tsx 	new file:   src/components/layout/Layout.css 	new file:   src/components/layout/Layout.tsx 	modified:   src/components/layout/Sidebar.tsx 	new file:   src/components/students/EditStudentModal.tsx 	new file:   src/components/students/FeeDueReminder.tsx 	new file:   src/components/students/ReceiptModal.tsx 	new file:   src/components/students/StudentAttendance.tsx 	new file:   src/components/students/StudentFeeHistory.tsx 	new file:   src/components/students/StudentFeesComponent.tsx 	new file:   src/components/students/StudentPerformance.tsx 	new file:   src/components/students/sendWhatsAppMessagesToDueStudents.tsx 	new file:   src/components/ui/EmptyState.tsx 	new file:   src/components/ui/ErrorBoundary.tsx 	new file:   src/components/ui/LoadingSpinner.tsx 	new file:   src/components/ui/Pagination.tsx 	new file:   src/components/ui/TabNav.tsx 	modified:   src/context/DatabaseContext.tsx 	modified:   src/context/UserContext.tsx 	new file:   src/data/addUser.jsx 	new file:   src/hooks/useStudents.ts 	new file:   src/pages/Attendance.tsx 	new file:   src/pages/Courses.tsx 	new file:   src/pages/Dashboard.tsx 	new file:   src/pages/Fees.tsx 	new file:   src/pages/Login.tsx 	new file:   src/pages/Performance.tsx 	new file:   src/pages/ScheduleExam.tsx 	new file:   src/pages/Settings.tsx 	new file:   src/pages/StudentTemp.tsx 	new file:   src/pages/temp.tsx 	new file:   src/utils/groupStudents.ts 	new file:   tailwind.config.js 	new file:   tsconfig.app.json 	modified:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu } from 'lucide-react';
+import {Search, Menu } from 'lucide-react';
 import { useUser } from '../../context/UserContext';
 interface HeaderProps {
   openSidebar: () => void;
